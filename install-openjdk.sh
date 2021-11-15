85940e065bc70b8c747a94d2e6554b9b69316bfd
jiangyayu <jiangyayu@cmhi.chinamobile.com>
2021-11-15 15:31:58 +0800

    install open jdk

--- install-openjdk.sh
+++ install-openjdk.sh
@@ -17,17 +17,17 @@ then
     yum install -y java-1.8.0-openjdk
     yum install -y java-1.8.0-openjdk-devel.x86_64
     echo "Set Environment variable..."
-    cat > /etc/profile.d/maven.sh << EOF
+    cat > /etc/profile.d/openjdk.sh << EOF
     #!/bin/bash
 
     export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.242.b08-0.el7_7.x86_64
     export JRE_HOME=$JAVA_HOME/jre
     export CLASSPATH=$JAVA_HOME/lib:$JRE_HOME/lib:$CLASSPATH
     export PATH=$JAVA_HOME/bin:$JRE_HOME/bin:$PATH
+EOF
 
-    EOF
-    source /etc/profile.d/maven.sh 
-    
+    source /etc/profile.d/openjdk.sh 
+
     hasJdk
     if [ $? != 1 ]
     then
