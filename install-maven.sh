8f872d7769a3d4c9c3f6d22a47f7ff2446ffd237
jiangyayu <jiangyayu@cmhi.chinamobile.com>
2021-11-16 09:58:46 +0800

    maven install script

---
+++ install-maven.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+
+# Color
+red='\033[31m'
+green='\033[32m'
+yellow='\033[33m'
+plain='\033[0m'
+
+exists(){
+    mvn -version
+    if [ $? -eq 0 ]
+    then
+        return 1;
+    else
+        return 0;
+    fi
+}
+
+exists
+if [ $? != 1 ]
+then
+    echo Download Maven
+    yum install -y wget 
+    wget https://mirrors.tuna.tsinghua.edu.cn/apache/maven/maven-3/3.8.3/binaries//apache-maven-3.8.3-bin.tar.gz  --no-check-certificate
+    tar -zxvf apache-maven-3.8.3-bin.tar.gz -C /usr/local -C /usr/local && rm -f apache-maven-3.8.3-bin.tar.gz
+    
+    cat > /etc/profile.d/maven.sh << \EOF
+#!/bin/bash
+
+export MAVEN_HOME=/usr/local/apache-maven-3.8.3
+export MAVEN_HOME
+export PATH=$PATH:$MAVEN_HOME/bin
+EOF
+
+    source /etc/profile.d/maven.sh 
+
+    exists
+    if [ $? == 1 ]
+    then
+        echo -e "${yellow}============================================================${plain}"
+        echo -e "${green}Maven Install Success${plain}"
+        echo -e "${yellow}============================================================${plain}"
+    else
+        echo -e "${yellow}============================================================${plain}"
+        echo -e "${red}Maven Install Failed ${plain}"
+        echo -e "${yellow}============================================================${plain}"
+    fi
+else
+    echo -e "${green}Maven Exists Already${plain}"
+
+fi
+echo ""
